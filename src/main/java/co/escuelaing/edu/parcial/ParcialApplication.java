da30b63eb8b137ac5c057332e341b2bec017cc63
unknown <zayra.gutierrez-s@labinfo.is.escuelaing.edu.co>
2025-10-22 09:39:29 -0500

    actualixar variable

--- src/main/java/co/escuelaing/edu/parcial/ParcialApplication.java
+++ src/main/java/co/escuelaing/edu/parcial/ParcialApplication.java
@@ -21,9 +21,9 @@ public class ParcialApplication {
 	}
 
 	@GetMapping("/primos")
-	public ResponseEntity<Map<String, Object>> obtenPrimos(@RequestParam(name = "x") int x) {
+	public ResponseEntity<Map<String, Object>> obtenPrimos(@RequestParam(name = "value") int value) {
 		List<Integer> primos = new ArrayList<>();
-		for (int i = 2; i <= x; i++) {
+		for (int i = 2; i <= value; i++) {
 			if (esPrimo(i)) {
 				primos.add(i);
 			}
@@ -31,7 +31,7 @@ public class ParcialApplication {
 		
 		Map<String, Object> resultadoJSON = new HashMap<>();
 		resultadoJSON.put("operation", "obtenPrimos");
-		resultadoJSON.put("input", x);
+		resultadoJSON.put("input", value);
 		resultadoJSON.put("output", primos);
 		return ResponseEntity.ok(resultadoJSON);
 	}
@@ -47,17 +47,17 @@ public class ParcialApplication {
     }
 
 	@GetMapping("/factores")
-	public ResponseEntity<Map<String, Object>> obtenFactores(@RequestParam(name = "x") int x){
+	public ResponseEntity<Map<String, Object>> obtenFactores(@RequestParam(name = "value") int value){
 		List<Integer> factores = new ArrayList<>();
-		for (int i = 1; i <= x; i++) {
-			if (x % i == 0) {
+		for (int i = 1; i <= value; i++) {
+			if (value % i == 0) {
 				factores.add(i);
 			}
 		}
 
 		Map<String, Object> resultadoJSON = new HashMap<>();
 		resultadoJSON.put("operation", "obtenFactores");
-		resultadoJSON.put("input", x);
+		resultadoJSON.put("input", value);
 		resultadoJSON.put("output", factores);
 		return ResponseEntity.ok(resultadoJSON);
 	}
